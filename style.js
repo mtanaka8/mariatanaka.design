d2b6a65288cdb74a4ae8cc6f3f022c5f49cec8aa
mtanaka8 <mtanaka8@hawaii.edu>
2023-02-22 23:06:08 -1000

    updated changes

---
+++ style.js
@@ -0,0 +1,21 @@
+var i = 0;
+var txt = 'Hi, I\'m Maria.';
+
+function typeOut(string) {
+  //begin delay for each letter typed
+  setTimeout(() => {
+    //only add letter or run function if the string is not done iterating.
+    if (i < string.length) {
+      //add character to dom here
+      document.getElementsByClassName('typewrite')[0].innerHTML += string.charAt(i);
+      //add iterator for next character in the string
+      i++;
+      //call the function once again to keep interation.
+      typeOut(txt);
+    }
+    //set the time delay
+  }, 150);
+}
+
+     
+typeOut(txt);
